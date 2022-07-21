0a123ba996746d97d74ca311f7cf1de498753931
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-07-21 11:28:55 -0300

    feat: getting metamask wallet on refresh

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react'
 import Web3 from 'web3'
 
+const web3 = new Web3(Web3.givenProvider || "https://localhost:8545")
+
 export default function Home(){
-    const web3 = new Web3(Web3.givenProvider || "https://localhost:8545")
+    
     const [account, setAccount] = useState(null)
 
-    async function callMetamask(){
-        setAccount(await web3.eth.requestAccounts())
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+                setAccount(await web3.eth.requestAccounts())
+            }
+
+        fetchData().catch(console.error)
+    
+    },[])
     
     return(
         <>  
-            <h1>Hey! Listen</h1>
             {account !== null ?<h1>{account}</h1> : <h1>Not connected</h1>}
-            <button onClick={callMetamask}>Connect to metamask</button>
+            <h1>Hey! Listen</h1>
+
         </>
         
     )
