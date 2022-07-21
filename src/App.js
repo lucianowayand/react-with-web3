ae5f1aebb4a40e400f746530b6aeebf26c93865f
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-07-21 12:06:28 -0300

    fix: navbar rendering before async function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useEffect, useState } from 'react'
+import Web3 from 'web3'
 import Home from "./pages/home";
 import NotFound from "./pages/not-found";
 
+const web3 = new Web3(Web3.givenProvider || "https://localhost:8545")
+
 export default function App() {
+    const [account, setAccount] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+                setAccount(await web3.eth.requestAccounts())
+
+            }
+
+        fetchData().catch(console.error)
+    
+    },[])
+    
+    function Navbar(){
+      return(
+        <div style={{ backgroundColor: '#3a3a3a', color:'white', display:'flex', justifyContent:'space-between', padding:'0.5rem 2rem' }}>
+            <h1>Test app</h1>
+            <h1>{String(account).substring(0,5)+"..."+String(account).substring(38)}</h1>
+        </div>
+      )
+    }
+
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="*" element={<NotFound />} />
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,10 @@
-import { useEffect, useState } from 'react'
-import Web3 from 'web3'
-import Navbar from '../../components/navbar'
-
-const web3 = new Web3(Web3.givenProvider || "https://localhost:8545")
 
 
 export default function Home(){
-    const [account, setAccount] = useState(null)
-    const [occludedAccount, setOccludedAccount] = useState("")
-
-    useEffect(() => {
-        const fetchData = async () => {
-                setAccount(await web3.eth.requestAccounts())
-                setOccludedAccount(String(account).substring(0,5)+"..."+String(account).substring(38))
-            }
-
-        fetchData().catch(console.error)
     
-    },[])
-    
-
-
     return(
         <>  
-            <Navbar occludedAccount={occludedAccount}/>
+            <h1>Hei</h1>
             
         </>
         
